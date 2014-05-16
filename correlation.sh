b14ea2a86960c477b62fb2e4826bd4d2f3c600ed
Michelle Liu <michelle@vapor.qb3.berkeley.edu>
2014-05-15 20:45:11 -0700

    script updated

--- correlation.sh
+++ correlation.sh
@@ -1,11 +1,11 @@
 skip=0
-total_time=400
-meas_time=200
+total_time=20000
+meas_time=1000
 sep=13.0
 #inp_file=../npt_6.8/C-walls_6.8_fix-rigid-npt_1atm_298K.t11136.traj
 inp_file=../C-walls_13.0_npt_1atm_298K.t28502.traj
 
-python correlation.py -s $skip -t $total_time -m $meas_time -d $sep $inp_file
+#python correlation.py -s $skip -t $total_time -m $meas_time -d $sep $inp_file
 ./correlation -s $skip -t $total_time -m $meas_time -d $sep $inp_file
 
 skip=0
